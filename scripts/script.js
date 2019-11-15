e7567eff721e078ac1997249dfc3d6f80cc57926
iguerrap <laley2003x@hotmail.com>
2019-11-14 20:53:01 -0600

    tres nuevos repositorios

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+ScrollReveal().reveal('.card-tex1');
+ScrollReveal().reveal('.card-tex2');
+ScrollReveal().reveal('.card-text3');
+ScrollReveal().reveal('.card-tex4');
+ScrollReveal().reveal('.card-img-top1');
+ScrollReveal().reveal('.card-img-top2');
+ScrollReveal().reveal('.card-img-top3');
+ScrollReveal().reveal('.card-img-top4');
+ScrollReveal().reveal('.card-title');
+//scroll con tiempo
+ScrollReveal().reveal('.card-tex1', { delay: 850 });
+ScrollReveal().reveal('.card-tex2', { delay: 850 });
+ScrollReveal().reveal('.card-text3', { delay: 850 });
+ScrollReveal().reveal('.card-tex4', { delay: 850 });
+ScrollReveal().reveal('.card-img-top1', { delay: 850 });
+ScrollReveal().reveal('.card-img-top2', { delay: 850 });
+ScrollReveal().reveal('.card-img-top3', { delay: 850 });
+ScrollReveal().reveal('.card-img-top4', { delay: 850 });
+ScrollReveal().reveal('.card-title', { delay: 850 });
+
